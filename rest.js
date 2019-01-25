a8f85f659fbfc3182acee1121ac090dde31d4765
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2019-01-24 15:23:49 -1000

    Add files via upload

--- rest.js
+++ rest.js
@@ -1,24 +1,28 @@
-
-  
-    var xmlhttp = new XMLHttpRequest();
+ var tax = 1.04712;
+ var subtotal=[];
+ var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
     if (this.readyState ==4 && this.status== 200){
-    var mydata= JSON.parse(this.responseText);//gets the json file and says its as mydata
-        document.getElementById("ingredents").innerHTML="mydata";
+    var menuData= JSON.parse(this.responseText);//gets the json file and saves its as mydata
+        
       }
     };
     xmlhttp.open("GET","bento.json",true);
     xmlhttp.send();
+document.getElementById("name").innerHTML=menuData.lumpia[0].type;
     //do this function when a criteria is met
     //4 means ready
     //200 means ok
    
-    function addItem()
-    {
-      var x=document.getElementById("userInput").value;
-       
-    }
-    function total(){
-      localStorage.setItem("quantity",x)
-        location.href="total.html";
-    }
+  
+  function buyItem(){
+    var order= document.getElementById("userInput").value;
+    var price= order*2.00;
+    var total=document.getElementById("quantity").innerHTML= "you ordered"+" "+"$"+price+ " of Banana Lumpia";
+    subtotal.push(total);
+  }
+  
+  function nextPage(){
+    var subtotal=[];
+    document.getElementById("display").innerHTML=subtotal;
+  }

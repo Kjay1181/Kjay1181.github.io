ce44a5fe665245e03ac1b43c66513dde7d7fb778
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2019-01-28 21:18:30 -1000

    Add files via upload

--- rest.js
+++ rest.js
@@ -1,15 +1,15 @@
- var tax = 1.04712;
- var subtotal=[];
+var tax = 1.04712;
+
  var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange=function(){
     if (this.readyState ==4 && this.status== 200){
-    var menuData= JSON.parse(this.responseText);//gets the json file and saves its as mydata
-        
+    var myData= JSON.parse(this.responseText);//gets the json file and saves its as mydata
+        document.getElementById("name").innerHTML=myData.lumpia[0].price;
       }
     };
     xmlhttp.open("GET","bento.json",true);
     xmlhttp.send();
-document.getElementById("name").innerHTML=menuData.lumpia[0].type;
+
     //do this function when a criteria is met
     //4 means ready
     //200 means ok
@@ -19,10 +19,11 @@ document.getElementById("name").innerHTML=menuData.lumpia[0].type;
     var order= document.getElementById("userInput").value;
     var price= order*2.00;
     var total=document.getElementById("quantity").innerHTML= "you ordered"+" "+"$"+price+ " of Banana Lumpia";
-    subtotal.push(total);
+  
   }
   
   function nextPage(){
-    var subtotal=[];
-    document.getElementById("display").innerHTML=subtotal;
+       var x=document.getElementById("userInput").value;//finds the value when button is clicked
+      localStorage.setItem("lumpia",x);// set the value
+      location.href="results.html";
   }

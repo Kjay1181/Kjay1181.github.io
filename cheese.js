44a73a92c279a466d5b7cd9ea62b6ec6468900af
Kevin D <42454194+Kjay1181@users.noreply.github.com>
2019-04-17 21:15:06 -0700

    Add files via upload

---
+++ cheese.js
@@ -0,0 +1,20 @@
+function cheese(){
+  this.x=Math.floor(Math.random()*w);
+  this.y=100;
+  this.width=50;
+  this.height=50;
+  this.count=0;
+  this.show=function(){
+    fill(color("green"));
+    ellipse(this.x,this.y,this.width,this.height);
+  }
+ this.score=function(){
+   d= dist(this.x, this.y, jumper.x, jumper.y);
+   if (d<50){
+     this.count++;
+      this.x=Math.floor(Math.random()*w);
+   }
+     
+   }
+ }
+
---
+++ platform.js
@@ -0,0 +1,29 @@
+var speed=7;// speed of the ship
+function platform(){
+  this.x=0-100;//starts off of the screen
+ this.y= Math.floor(Math.random()*h);// random point on the y axis
+  this.width=100;
+  this.height=100;
+  this.show=function(){
+    fill(color("green"));
+    rect(this.x,this.y,this.width,this.height);
+  }
+  this.update=function(){//makes the ball fall down
+ this.x= this.x+speed;
+ if (this.x > w){
+  
+  speed=-speed;
+  
+ }
+  
+    
+  }
+this.reverse= function(){
+  if(this.x<0){
+
+speed=7;
+  }
+
+}
+
+}
